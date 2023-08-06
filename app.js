45fd58fb9eb8287621d2821ffd76cde1f8e6b981
SkyAerope <81958172+SkyAerope@users.noreply.github.com>
2023-08-06 22:22:22 +0800

    feat: 基本功能实现

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+const readline = require('readline');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/api/random-word', async (req, res) => {
+    const fileStream = fs.createReadStream('Genshin.txt');
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let words = [];
+    for await (const word of rl) {
+        words.push(word);
+    }
+
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    res.send(randomWord);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
